2cdf50ec5655272b845de4669c0686f0356d07ea
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-09-04 14:18:06 -0600

    .

--- dockmol.py
+++ dockmol.py
@@ -10,18 +10,23 @@ subprocess.call('rm /app/dockmol/*.pdb', shell=True)
 st.title("inicio")     
 # Using object notation
 prot_pdb_name=''
+prot_pdb_id=''
 
 with st.sidebar:
     prot_pdb_or = st.selectbox("Elige origen del archivo pdb?",("PDB ID", "From PC"))
     if prot_pdb_or == "PDB ID":
         prot_pdb_id=st.text_input('PDB ID', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
-        if prot_pdb_id is not '':
+        if prot_pdb_id != '':
             prot_pdb_name=prot_pdb_id+'.pdb'
             st.write('---------',prot_pdb_name)
             prot_pdb_content = get_pdb_file(prot_pdb_id, filetype='pdb', compression=False)
             with open(prot_pdb_name,'w') as f:
                 print(prot_pdb_content,file=f)
-                subprocess.call('cat /app/dockmol/'+prot_pdb_name, shell=True)
+                subprocess.call('echo +++++++++++++++++++++++++++++++++++++++', shell=True)
+                subprocess.call('head /app/dockmol/'+prot_pdb_name, shell=True)
+                subprocess.call('echo ---------------------------------------', shell=True)
+                subprocess.call('ls /app/dockmol/', shell=True)
+                subprocess.call('echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-', shell=True)
 
  #       if prot_pdb_id is not '':
  #           pdbview = py3Dmol.view(query='pdb:'+prot_pdb_id) 
@@ -56,5 +61,5 @@ with st.sidebar:
 subprocess.call('ls /app/dockmol/', shell=True)
 subprocess.call('echo +++++++++++++++++++++++++++++++++++++++', shell=True)
 subprocess.call('echo +++++++++++++++++++++++++++++++++++++++', shell=True)
-if prot_pdb_name is not '':
-    subprocess.call('cat /app/dockmol/'+prot_pdb_name, shell=True)
+#if prot_pdb_name != '':
+#    subprocess.call('cat /app/dockmol/'+prot_pdb_name, shell=True)
