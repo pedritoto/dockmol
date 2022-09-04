fece56a1dd340ff94fc5dd7fbcf244484fbfafbb
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-09-04 11:44:03 -0600

    .

--- dockmol.py
+++ dockmol.py
@@ -8,14 +8,16 @@ st.title("inicio")
 from io import StringIO
 prot_pdb_uploaded=st.file_uploader("sube pdb", type='pdb', accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 if prot_pdb_uploaded is not None:
-    with open(prot_pdb_uploaded.name,'wb') as f:
+    prot_pdb_name=prot_pdb_uploaded.name
+    with open(prot_pdb_name,'wb') as f:
         f.write(prot_pdb_uploaded.getbuffer())
-        mol = pybel.readfile("pdb",prot_pdb_uploaded.name)
+        mol = pybel.readfile("pdb",prot_pdb_name)
         # 1A2C
         # Structure of thrombin inhibited by AERUGINOSIN298-A from a BLUE-GREEN ALGA
-    xyzview = py3Dmol.view(prot_pdb_uploaded.name) 
-    xyzview.setStyle('licorice')
-    showmol(xyzview, height = 500,width=800)
+    pdbview = py3Dmol.view() 
+    pdbview.addModel(open(prot_pdb_name, 'r').read(),'pdb')
+    pdbview.setStyle({'stick':{}})
+    showmol(pdbview, height = 500,width=800)
 
 
 
