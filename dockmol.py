61c1f6e6a7383c056141ff5f2d020fb53fd7ac94
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-09-04 00:41:06 -0600

    .

--- dockmol.py
+++ dockmol.py
@@ -2,8 +2,8 @@ import streamlit as st
 from openbabel import pybel
 st.title("inicio")     
 from io import StringIO
-
-stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+file=st.file_uploader("sube pdb", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+stringio = StringIO(file.getvalue().decode("utf-8"))
 st.write(stringio)
 
 #file=st.file_uploader("sube pdb", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
