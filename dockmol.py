0b599fed757c23fd4305c9a9efc45f54e35d972b
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-09-04 11:28:05 -0600

    .

--- dockmol.py
+++ dockmol.py
@@ -4,9 +4,9 @@ st.title("inicio")
 from io import StringIO
 prot_pdb_uploaded=st.file_uploader("sube pdb", type='pdb', accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 if prot_pdb_uploaded is not None:
-    with open(prot_pdb_uploaded.name,'wb') as prot_pdb_name:
-        prot_pdb_name.write(prot_pdb_uploaded.getbuffer())
-        mol = pybel.readfile("pdb",prot_pdb_name)
+    with open(prot_pdb_uploaded.name,'wb') as f:
+        f.write(prot_pdb_uploaded.getbuffer())
+        mol = pybel.readfile("pdb",prot_pdb_uploaded.name)
 import subprocess
 
 cmd = "pwd "
