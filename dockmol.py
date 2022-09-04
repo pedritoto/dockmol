7374f69f80250c3e8eec45dceb1433431ee64ec5
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-09-04 00:20:49 -0600

    .

--- dockmol.py
+++ dockmol.py
@@ -1,4 +1,4 @@
 import streamlit as st
 from openbabel import pybel
 st.title("inicio")
-st.file_uploader(label, type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False)
+st.file_uploader(label, type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
