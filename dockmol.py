67bfca04d12bc69fd09c96b51d1022160f59268f
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-09-04 14:26:51 -0600

    .

--- dockmol.py
+++ dockmol.py
@@ -12,38 +12,39 @@ st.title("inicio")
 prot_pdb_name=''
 prot_pdb_id=''
 
+
 with st.sidebar:
-    prot_pdb_or = st.selectbox("Elige origen del archivo pdb?",("PDB ID", "From PC"))
-    if prot_pdb_or == "PDB ID":
-        prot_pdb_id=st.text_input('PDB ID', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
-        if prot_pdb_id != '':
-            prot_pdb_name=prot_pdb_id+'.pdb'
-            st.write('---------',prot_pdb_name)
-            prot_pdb_content = get_pdb_file(prot_pdb_id, filetype='pdb', compression=False)
-            with open(prot_pdb_name,'w') as f:
-                print(prot_pdb_content,file=f)
-            subprocess.call('echo ++++++++++++  FROM RSCB   ++++++++++++++', shell=True)
-            subprocess.call('head /app/dockmol/'+prot_pdb_name, shell=True)
-            subprocess.call('echo ---------------------------------------', shell=True)
-            subprocess.call('ls /app/dockmol/', shell=True)
-            subprocess.call('echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-', shell=True)
+    with st.expander("Prtotein"):
+        prot_pdb_or = st.selectbox("Elige origen del archivo pdb?",("PDB ID", "From PC"))
+        if prot_pdb_or == "PDB ID":
+            prot_pdb_id=st.text_input('PDB ID', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
+            if prot_pdb_id != '':
+                prot_pdb_name=prot_pdb_id+'.pdb'
+                prot_pdb_content = get_pdb_file(prot_pdb_id, filetype='pdb', compression=False)
+                with open(prot_pdb_name,'w') as f:
+                    print(prot_pdb_content,file=f)
+                subprocess.call('echo ++++++++++++  FROM RSCB   ++++++++++++++', shell=True)
+                subprocess.call('head /app/dockmol/'+prot_pdb_name, shell=True)
+                subprocess.call('echo ---------------------------------------', shell=True)
+                subprocess.call('ls /app/dockmol/', shell=True)
+                subprocess.call('echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-', shell=True)
 
  #       if prot_pdb_id is not '':
  #           pdbview = py3Dmol.view(query='pdb:'+prot_pdb_id) 
  #           pdbview.setStyle({'cartoon':{'color':'spectrum'}})
  #           pdbview.zoomTo()
  #           showmol(pdbview, height = 500,width=800)
-    else:
-        prot_pdb_uploaded=st.file_uploader("sube pdb", type='pdb', accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
-        if prot_pdb_uploaded is not None:
-            prot_pdb_name=prot_pdb_uploaded.name
-            with open(prot_pdb_name,'wb') as f:
-                f.write(prot_pdb_uploaded.getbuffer()) 
-            subprocess.call('echo ++++++++++++ SUBIDO ++++++++++++++++++', shell=True)
-            subprocess.call('head /app/dockmol/'+prot_pdb_name, shell=True)
-            subprocess.call('echo ---------------------------------------', shell=True)
-            subprocess.call('ls /app/dockmol/', shell=True)
-            subprocess.call('echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-', shell=True)                 
+        else:
+            prot_pdb_uploaded=st.file_uploader("sube pdb", type='pdb', accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+            if prot_pdb_uploaded is not None:
+                prot_pdb_name=prot_pdb_uploaded.name
+                with open(prot_pdb_name,'wb') as f:
+                    f.write(prot_pdb_uploaded.getbuffer()) 
+                subprocess.call('echo ++++++++++++ SUBIDO ++++++++++++++++++', shell=True)
+                subprocess.call('head /app/dockmol/'+prot_pdb_name, shell=True)
+                subprocess.call('echo ---------------------------------------', shell=True)
+                subprocess.call('ls /app/dockmol/', shell=True)
+                subprocess.call('echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-', shell=True)                 
             #prot_pdb_content = open(prot_pdb_name, 'r').read()
             #prot_pdb_content
             #subprocess.call('cat /app/dockmol/'+prot_pdb_name, shell=True)
