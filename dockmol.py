e4900fe7a90360d36af8dfe62e360f26668741a9
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-09-04 00:26:16 -0600

    .

--- dockmol.py
+++ dockmol.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from openbabel import pybel
 st.title("inicio")
-st.file_uploader("sube pdb", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
-mol = pybel.readfile("pdb", "water.pdb")
+file=st.file_uploader("sube pdb", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+st.write(file)
+#mol = pybel.readfile("pdb", "water.pdb")
