9c4bfc109f81cd5755f13feea97e56570abbc6ec
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-09-04 10:34:52 -0600

    .

--- dockmol.py
+++ dockmol.py
@@ -3,16 +3,16 @@ from openbabel import pybel
 st.title("inicio")     
 from io import StringIO
 fileup=st.file_uploader("sube pdb", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
-if fileup is not None:
-    stringio = StringIO(fileup.getvalue().decode("utf-8"))
-    st.write(stringio)
+#if fileup is not None:
+#    stringio = StringIO(fileup.getvalue().decode("utf-8"))
+#    st.write(stringio)
 
-#file=st.file_uploader("sube pdb", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
-#st.write(file)
+file=st.file_uploader("sube pdb", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+st.write(file)
 #mol = pybel.readfile("pdb", "water.pdb")
 import subprocess
 
-cmd = "ls "
+cmd = "pwd "
 
 subprocess.call(cmd, shell=True)
 
