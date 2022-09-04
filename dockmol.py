723745f164f95b9e632281da2b78073d28d79602
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-09-04 10:35:11 -0600

    .

--- dockmol.py
+++ dockmol.py
@@ -7,7 +7,7 @@ fileup=st.file_uploader("sube pdb", type=None, accept_multiple_files=True, key=N
 #    stringio = StringIO(fileup.getvalue().decode("utf-8"))
 #    st.write(stringio)
 
-file=st.file_uploader("sube pdb", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+#file=st.file_uploader("sube pdb", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 st.write(file)
 #mol = pybel.readfile("pdb", "water.pdb")
 import subprocess
