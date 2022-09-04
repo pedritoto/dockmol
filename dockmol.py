d703292817e0b5d995562112b483cd74bf30f6b7
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-09-04 12:27:05 -0600

    .

--- dockmol.py
+++ dockmol.py
@@ -21,7 +21,7 @@ if prot_pdb_uploaded is not None:
    
 pdbview = py3Dmol.view()
 pdbview.addModel(open(prot_pdb_name, 'r').read(),'pdb')
-pdbview.setStyle({'stick':{}})
+pdbview.setStyle({'CPK':{}})
 pdbview.zoomTo()
 showmol(pdbview, height = 500,width=800)
 #lines=open(prot_pdb_name, 'r').read()
