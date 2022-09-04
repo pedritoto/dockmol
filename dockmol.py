0b0dc00ace075264b715a66107aafb785261782b
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-09-04 12:35:34 -0600

    .

--- dockmol.py
+++ dockmol.py
@@ -8,18 +8,18 @@ import subprocess
 subprocess.call('rm /app/dockmol/*.pdb', shell=True)
 
 st.title("inicio")     
-from io import StringIO
-prot_pdb_uploaded=st.file_uploader("sube pdb", type='pdb', accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
-if prot_pdb_uploaded is not None:
-    prot_pdb_name=prot_pdb_uploaded.name
-    with open(prot_pdb_name,'wb') as f:
-        f.write(prot_pdb_uploaded.getbuffer())
+with st.sidebar:
+    prot_pdb_uploaded=st.file_uploader("sube pdb", type='pdb', accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+    if prot_pdb_uploaded is not None:
+        prot_pdb_name=prot_pdb_uploaded.name
+        with open(prot_pdb_name,'wb') as f:
+            f.write(prot_pdb_uploaded.getbuffer())
         #mol = pybel.readfile("pdb",prot_pdb_name)
-    pdbview = py3Dmol.view()
-    pdbview.addModel(open(prot_pdb_name, 'r').read(),'pdb')
-    pdbview.setStyle({'stick':{}})
-    pdbview.zoomTo()
-    showmol(pdbview, height = 500,width=800)
+        pdbview = py3Dmol.view()
+        pdbview.addModel(open(prot_pdb_name, 'r').read(),'pdb')
+        pdbview.setStyle({'stick':{}})
+        pdbview.zoomTo()
+        showmol(pdbview, height = 500,width=800)
  
    
 
