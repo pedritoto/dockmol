1fe2ed01a4f0003f07e37f3825c10170cb461c44
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-09-04 11:31:57 -0600

    .

--- dockmol.py
+++ dockmol.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from openbabel import pybel
+
+from stmol import showmol
+import py3Dmol
+
 st.title("inicio")     
 from io import StringIO
 prot_pdb_uploaded=st.file_uploader("sube pdb", type='pdb', accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
@@ -7,10 +11,16 @@ if prot_pdb_uploaded is not None:
     with open(prot_pdb_uploaded.name,'wb') as f:
         f.write(prot_pdb_uploaded.getbuffer())
         mol = pybel.readfile("pdb",prot_pdb_uploaded.name)
+        # 1A2C
+        # Structure of thrombin inhibited by AERUGINOSIN298-A from a BLUE-GREEN ALGA
+        xyzview = py3Dmol.view(mol) 
+        xyzview.setStyle({'cartoon':{'color':'spectrum'}})
+        showmol(xyzview, height = 500,width=800)
+
+
+
+
+
+
 import subprocess
-
-cmd = "pwd "
-
-subprocess.call(cmd, shell=True)
-
 subprocess.call('ls /app/dockmol/', shell=True)
