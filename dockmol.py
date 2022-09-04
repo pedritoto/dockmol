a49f37ccc3645f7a992f0e0e2190026594f5b37b
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-09-04 00:42:52 -0600

    .

--- dockmol.py
+++ dockmol.py
@@ -3,8 +3,9 @@ from openbabel import pybel
 st.title("inicio")     
 from io import StringIO
 file=st.file_uploader("sube pdb", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
-stringio = StringIO(file.getvalue().decode("utf-8"))
-st.write(stringio)
+if file not None:
+    stringio = StringIO(file.getvalue().decode("utf-8"))
+    st.write(stringio)
 
 #file=st.file_uploader("sube pdb", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 #st.write(file)
